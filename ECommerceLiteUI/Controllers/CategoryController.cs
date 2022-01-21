43613630ba6947c0acdc47fd1f0f736245e5d290
merveakin <merveakinnnn@hotmail.com>
2022-01-21 12:30:05 +0300

    CategoryController'a ait sayfalar eklendi.

--- ECommerceLiteUI/Controllers/CategoryController.cs
+++ ECommerceLiteUI/Controllers/CategoryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ECommerceLiteBLL.Repository;
+using ECommerceLiteEntity.Models;
 
 namespace ECommerceLiteUI.Controllers
 {
@@ -11,7 +12,6 @@ namespace ECommerceLiteUI.Controllers
     {
         //GLOBAL ZONE
         CategoryRepo myCategoryRepo = new CategoryRepo();
-
         // GET: Category
         public ActionResult CategoryList()
         {
@@ -19,5 +19,80 @@ namespace ECommerceLiteUI.Controllers
             ViewBag.CategoryCount = allCategories.Count;
             return View(allCategories);
         }
+
+        public ActionResult Create(int? id)
+        {
+            if (id != null)
+            {
+                Category model = new Category()
+                {
+                    Id = id.Value
+                };
+                return View(model);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Category model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    ModelState.AddModelError("", "Invalid data entry! Please enter the data correctly!");
+                    return View(model);
+                }
+
+                Category newCategory = new Category()
+                {
+                    CategoryName = model.CategoryName,
+                    CategoryDescription = model.CategoryDescription,
+                    RegisterDate = DateTime.Now,
+                    BaseCategoryId = null
+                };
+
+                if (model.Id > 0)
+                {
+                    newCategory.BaseCategoryId = model.Id;
+                }
+                int insertResult = myCategoryRepo.Insert(newCategory);
+                if (model.Id > 0
+                    && model.Id == 0)
+                {
+                    return RedirectToAction("CategoryList", "Category");
+                }
+
+                else if (insertResult > 0 && model.Id > 0)
+                {
+                    return RedirectToAction("SubCategoryList", "Category", new { id=model.Id});
+                }
+
+                else
+                {
+                    throw new Exception("Error adding category! Unable to add category!");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                ModelState.AddModelError("", "Unexpected Error! Try Again! HATA :" + ex.Message);
+                //TODO : Ex loglanacak
+                return View(model);
+            }
+        }
+
+        public ActionResult SubCategoryList(int id)
+        {
+            var subCategories = myCategoryRepo.Queryable().Where(x => x.BaseCategoryId != null
+            && x.BaseCategoryId == id).ToList();
+
+            ViewBag.CategoryId = id;
+            ViewBag.CategoryName = myCategoryRepo.GetById(id).CategoryName;
+            ViewBag.CategoryCount = subCategories.Count;
+            return View(subCategories);
+        }
+
     }
 }
