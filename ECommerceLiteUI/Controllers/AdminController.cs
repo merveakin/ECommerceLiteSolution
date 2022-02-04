770e635aefce8b40af9bf07c6ef1518de8cf7e23
merveakin <merveakinnnn@hotmail.com>
2022-02-04 14:44:49 +0300

    Dashboard2 sayfasına donut piechart eklendi.

--- ECommerceLiteUI/Controllers/AdminController.cs
+++ ECommerceLiteUI/Controllers/AdminController.cs
@@ -12,7 +12,7 @@ namespace ECommerceLiteUI.Controllers
     {
         //GLOBAL ZONE
         OrderRepo myOrderRepo = new OrderRepo();
-        
+        CategoryRepo myCategoryRepo = new CategoryRepo();
         // GET: Admin
         public ActionResult Dashboard()
         {
@@ -24,6 +24,21 @@ namespace ECommerceLiteUI.Controllers
 
             return View();
         }
+        
+        public ActionResult Dashboard2()
+        {
+            var OrderList = myOrderRepo.GetAll();
+            //1 aylık sipariş sayısı >>>
+            var newOrderCount = OrderList.Where(x => x.RegisterDate >= DateTime.Now.AddMonths(-1)).ToList().Count;
+            ViewBag.NewOrderCount = newOrderCount;
+
+            var model = myCategoryRepo.GetBaseCategoriesProductCount();
+            return View(model);
+        }
+
+
+
+        [AllowAnonymous]
         public ActionResult Deneme()
         {
             return View();
