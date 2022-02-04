18d3a2a9089cb8af336af375eef362dc88c857be
merveakin <merveakinnnn@hotmail.com>
2022-02-04 12:43:31 +0300

    Edit Ekranı HttpPost Model is valid düzenlendi.

--- ECommerceLiteUI/Controllers/ProductController.cs
+++ ECommerceLiteUI/Controllers/ProductController.cs
@@ -23,6 +23,16 @@ namespace ECommerceLiteUI.Controllers
 
         public ActionResult ProductList(int page = 1, string search = "", bool isNew = false)
         {
+            List<SelectListItem> subCategories = new List<SelectListItem>();
+            myCategoryRepo.Queryable()
+                .Where(x => x.BaseCategoryId != null)
+                .ToList().ForEach(x => subCategories.Add(new SelectListItem()
+                {
+                    Text = x.CategoryName,
+                    Value = x.Id.ToString()
+                }));
+            ViewBag.CategoryList = subCategories;
+
             List<Product> allProductList = new List<Product>();
 
             if (string.IsNullOrEmpty(search))
@@ -46,7 +56,7 @@ namespace ECommerceLiteUI.Controllers
             }
 
             var user = MembershipTools.GetNameSurname();
-            LogManager.LogMessage("geldi", userInfo: user,pageInfo:"Product/ProductList");
+            LogManager.LogMessage("geldi", userInfo: user, pageInfo: "Product/ProductList");
             return View(allProductList.ToPagedList(page, 3));
         }
 
@@ -171,7 +181,7 @@ namespace ECommerceLiteUI.Controllers
                 else
                 {
                     ModelState.AddModelError("", "Error occurred while adding a product!Try Again!");
-                   //ex loglanacak
+                    //ex loglanacak
                     return View(model);
                 }
             }
@@ -211,15 +221,89 @@ namespace ECommerceLiteUI.Controllers
         {
             try
             {
+                if (!ModelState.IsValid)
+                {
+                    TempData["ProductModalError"] = "Data entries must be correct!";
+                    return RedirectToAction("ProductList", "Product");
+                }
                 var product = myProductRepo.GetById(model.Id);
                 product.ProductName = model.ProductName;
                 product.Description = model.Description;
                 product.Quantity = model.Quantity;
                 product.Price = model.Price;
-                if (model.Files.Any())
-                {
-                    //
-                }
+                product.CategoryId = model.CategoryId;
+                //if (model.Files.Any())
+                //{
+                //    //Önce sildik
+                //    var pictureList =
+                //          myProductPictureRepo.Queryable()
+                //          .Where(x => x.ProductId == model.Id).ToList();
+                //    foreach (var item in pictureList)
+                //    {
+                //        myProductPictureRepo.Delete(item);
+                //    }
+                //    //sonra yeni eklediklerini oluşturacağız.
+                //    ProductPicture productPicture = new ProductPicture();
+                //    productPicture.ProductId = model.Id;
+                //    productPicture.RegisterDate = DateTime.Now;
+                //    int counter = 1;
+                //    foreach (var item in model.Files)
+                //    {
+
+                //        if (item != null && item.ContentType.Contains("image") && item.ContentLength > 0)
+                //        {
+
+                //            string filename = SiteSettings.UrlFormatConverter(model.ProductName).ToLower().Replace("-", "");
+                //            string extName = Path
+                //                .GetExtension(item.FileName);
+
+                //            string guid = Guid.NewGuid()
+                //                .ToString().Replace("-", "");
+                //            var directoryPath = Server.MapPath($"~/ProductPictures/{filename}/{model.ProductCode}");
+                //            var filePath = Server.MapPath($"~/ProductPictures/{filename}/{model.ProductCode}/") + filename + counter + "-" + guid + extName;
+                //            if (!Directory.Exists(directoryPath))
+                //            {
+                //                Directory.CreateDirectory(directoryPath);
+                //            }
+                //            item.SaveAs(filePath);
+                //            if (counter == 1)
+                //            {
+                //                productPicture.ProductPicture1 = $"/ProductPictures/{filename}/{model.ProductCode}/" + filename + counter + "-" + guid + extName;
+                //            }
+                //            if (counter == 2)
+                //            {
+                //                productPicture.ProductPicture2 = $"/ProductPictures/{filename}/{model.ProductCode}/" + filename + counter + "-" + guid + extName;
+                //            }
+                //            if (counter == 3)
+                //            {
+                //                productPicture.ProductPicture3 = $"/ProductPictures/{filename}/{model.ProductCode}/" + filename + counter + "-" + guid + extName;
+                //            }
+                //            if (counter == 4)
+                //            {
+                //                productPicture.ProductPicture4 = $"/ProductPictures/{filename}/{model.ProductCode}/" + filename + counter + "-" + guid + extName;
+                //            }
+                //            if (counter == 5)
+                //            {
+                //                productPicture.ProductPicture5 = $"/ProductPictures/{filename}/{model.ProductCode}/" + filename + counter + "-" + guid + extName;
+                //            }
+
+
+                //        }
+                //        counter++;
+                //    }
+
+                //    int pictureInsertResult =
+                //        myProductPictureRepo.Insert(productPicture);
+                //    if (pictureInsertResult == 0)
+                //    {
+                //        TempData["ProductModalError"] = "The product has been added, but an error occurred while adding the photos of the product.Try again to add a photo!";
+                //    }
+                //    else
+                //    {
+                //        TempData["ProductModalError"] = string.Empty;
+                //    }
+                //}
+
                 int updateResult = myProductRepo.Update();
                 if (updateResult > 0)
                 {
@@ -229,21 +313,19 @@ namespace ECommerceLiteUI.Controllers
                 {
                     //geçici return
                     return RedirectToAction("ProductList", "Product");
+
                 }
+
             }
             catch (Exception ex)
             {
-
-                //ex loglanacak+++
                 var user = MembershipTools.GetNameSurname();
                 LogManager.LogMessage(ex.ToString(),
                     userInfo: user, pageInfo: "Product/Edit");
-
+                //geçici
                 return RedirectToAction("ProductList", "Product");
-
             }
         }
-
         public JsonResult GetProductDetails(int id)
         {
             var product = myProductRepo.GetById(id);
